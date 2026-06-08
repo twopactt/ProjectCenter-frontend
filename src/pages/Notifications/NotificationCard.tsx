fb2fdbe4103637c33e4a6d61c0797b25e4ff7bf5
twopact <artemov9321@mail.ru>
2026-06-09 00:09:02 +0300

    Доделан вывод и чтение уведомлений

--- src/pages/Notifications/NotificationCard.tsx
+++ src/pages/Notifications/NotificationCard.tsx
@@ -1,56 +1,76 @@
-import { Card, CardHeader, CardBody, DataList } from '@chakra-ui/react'
+import {
+	Card,
+	CardBody,
+	Stack,
+	Text,
+	Badge,
+	Button,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@chakra-ui/react'
+import type { NotificationResponse } from '@/shared/types/notification'
+import { LuCheckCheck } from 'react-icons/lu'
 import moment from 'moment/moment'
 import 'moment/locale/ru'
 
 moment.locale('ru')
 
 interface NotificationCardProps {
-	id: number
-	title: string
-	text: string
-	createdAt: string
-	isRead: boolean
-	typeName: string
+	notification: NotificationResponse
+	onNotificationAsRead: (id: number) => void
 }
 
 function NotificationCard({
-	title,
-	text,
-	createdAt,
-	isRead,
-	typeName,
+	notification,
+	onNotificationAsRead,
 }: NotificationCardProps) {
+	const isUnread = !notification.isRead
+
 	return (
 		<Card.Root
-			_hover={{ bg: 'gray.100', _dark: { bg: 'gray.800' } }}
+			className='transition-all'
+			bg={isUnread ? 'blue.50' : ''}
+			_dark={{ bg: isUnread ? 'blue.900/50' : '' }}
+			_hover={{
+				bg: isUnread ? 'blue.100' : 'gray.100',
+				_dark: { bg: isUnread ? 'blue.900/90' : 'gray.800' },
+			}}
 			borderWidth='1px'
 		>
 			<CardHeader>
-				<Card.Title>{title}</Card.Title>
+				<div className='flex justify-between items-start flex-wrap gap-2'>
+					<div className='flex items-center gap-3'>
+						<CardTitle>{notification.title}</CardTitle>
+					</div>
+					<div className='flex items-center gap-2'>
+						<Text fontSize='sm' color='gray.500'>
+							{moment(notification.createdAt).startOf('minute').fromNow()}
+						</Text>
+						{isUnread && (
+							<Button
+								size='xs'
+								variant='ghost'
+								colorPalette='blue'
+								onClick={() => onNotificationAsRead(notification.id)}
+							>
+								Прочитать
+								<LuCheckCheck />
+							</Button>
+						)}
+					</div>
+				</div>
 			</CardHeader>
-
-			<CardBody className='flex flex-col gap-1'>
-				<DataList.Root className='flex-wrap' orientation='horizontal'>
-					<DataList.Item>
-						<DataList.ItemLabel>Текст</DataList.ItemLabel>
-						<DataList.ItemValue>{text}</DataList.ItemValue>
-					</DataList.Item>
-					<DataList.Item>
-						<DataList.ItemLabel>Тип</DataList.ItemLabel>
-						<DataList.ItemValue>{typeName}</DataList.ItemValue>
-					</DataList.Item>
-					<DataList.Item>
-						<DataList.ItemLabel>Когда</DataList.ItemLabel>
-						<DataList.ItemValue>
-							{moment(createdAt).startOf('minute').fromNow()}
-						</DataList.ItemValue>
-					</DataList.Item>
-					<DataList.Item>
-						<DataList.ItemLabel>Прочитано</DataList.ItemLabel>
-						<DataList.ItemValue>{isRead ? 'да' : 'нет'}</DataList.ItemValue>
-					</DataList.Item>
-				</DataList.Root>
+			<CardBody>
+				<Stack gap={2}>
+					<Text fontSize='md'>{notification.text}</Text>
+				</Stack>
 			</CardBody>
+			<CardFooter>
+				<Badge colorPalette='green' alignSelf='flex-start'>
+					{notification.typeName}
+				</Badge>
+			</CardFooter>
 		</Card.Root>
 	)
 }
