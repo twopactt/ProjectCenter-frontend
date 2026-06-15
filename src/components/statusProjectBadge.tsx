5b685a66171fec3c1a0d8bc96f54398ff5b3b53e
twopact <artemov9321@mail.ru>
2026-06-16 00:29:00 +0300

    Исправлен баг с адаптивом

--- src/components/statusProjectBadge.tsx
+++ src/components/statusProjectBadge.tsx
@@ -1,10 +1,14 @@
-import { Badge } from "@chakra-ui/react"
-import { getStatusColor } from "../shared/utils/statusProjectColors"
+import { Badge } from '@chakra-ui/react'
+import { getStatusColor } from '../shared/utils/statusProjectColors'
 
 interface StatusProjectBadgeProps {
 	status: string
 }
 
 export function StatusProjectBadge({ status }: StatusProjectBadgeProps) {
-	return <Badge colorPalette={getStatusColor(status)}>{status}</Badge>
+	return (
+		<Badge className='text-wrap' colorPalette={getStatusColor(status)}>
+			{status}
+		</Badge>
+	)
 }
